a2b0654b3f8f1233cff919902d29cd1aa38662a0
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-06-22 22:54:28 +0300

    git-push

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 function App() {
   return (
     <div>
-      
+      <h1>
+        Hello React
+      </h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { hover } from '@testing-library/user-event/dist/hover';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
     <App />
- 
+    
 );
